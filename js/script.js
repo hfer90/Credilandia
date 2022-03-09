cb31e804e65af382c3ee94d69e44b63f1bdfcaed
hfer90 <95000349+hfer90@users.noreply.github.com>
2022-03-08 22:10:42 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ const cuotas = document.getElementById('cuotas');
 const generar = document.getElementById('generar');
 const guardar = document.getElementById('guardar');
 const cargar = document.getElementById('cargar');
+const llenarTabla = document.querySelector('#lista-tabla tbody');
 
 function prestamo(monto, cuotas, tasa, valorCuota) {
-    this.monto = parseFloat(monto);
+    this.monto = parseInt(monto);
     this.cuotas = parseInt(cuotas);
     this.tasa = tasa;
     this.valorCuota = valorCuota;
@@ -15,12 +16,47 @@ var prestamo1 = new prestamo(0, 0, 0, 0);
 
 $(document).ready(function () {
 
-    function calcularTasa() {
+    function calcularTasa() { 
         if (prestamo1.cuotas > 24) { prestamo1.tasa = 50; } else { prestamo1.tasa = 45; }
     }
 
     function calcularCuota(monto, cuotas, tasa) {
-        prestamo1.valorCuota = parseInt(monto * (Math.pow(1 + tasa / 100, cuotas) * tasa / 100) / (Math.pow(1 + tasa / 100, cuotas) - 1));
+
+        while(llenarTabla.firstChild){
+            llenarTabla.removeChild(llenarTabla.firstChild);
+        }
+    
+        let fechas = [];
+        let fechaActual = Date.now();
+        let mes_actual = moment(fechaActual);
+        mes_actual.add(1, 'month');    
+    
+        let interes = 0, capital = 0;
+    
+        prestamo1.valorCuota = parseFloat(monto * (Math.pow(1 + tasa / 100, cuotas) * tasa / 100) / (Math.pow(1 + tasa / 100, cuotas) - 1));
+    
+        for(let i = 1; i <= prestamo1.cuotas; i++) {
+    
+            interes = parseFloat(monto*(tasa / 100));
+            capital = prestamo1.valorCuota - interes;
+            monto = parseFloat(monto-capital);
+    
+            //Formato fechas
+            fechas[i] = mes_actual.format('DD-MM-YYYY');
+            mes_actual.add(1, 'month');
+    
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${fechas[i]}</td>
+                <td>${prestamo1.valorCuota.toFixed(2)}</td>
+                <td>${capital.toFixed(2)}</td>
+                <td>${interes.toFixed(2)}</td>
+                <td>${monto.toFixed(2)}</td>
+            `;
+            llenarTabla.appendChild(row)
+        }
+    
+    
     }
 
     function imprimirResultado() {
@@ -59,21 +95,24 @@ $(document).ready(function () {
     });
 
     $('#generar').on('click', () => {
-        calcularTasa();
-        calcularCuota(prestamo1.monto, prestamo1.cuotas, prestamo1.tasa / 12);
-        imprimirResultado();
-        animacion();
+        if (monto.value > 0 && cuotas.value > 0){
+            calcularTasa();
+            calcularCuota(prestamo1.monto, prestamo1.cuotas, prestamo1.tasa / 12);
+            imprimirResultado();
+            animacion();
+        } else {alert("Por favor ingrese correctamente monto y cuotas.")}
     });
 
     guardar.addEventListener('click', () => {
         guardarPrestamo();
-
     });
 
     cargar.addEventListener('click', () => {
-        cargarPrestamo();
-        imprimirResultado();
-        animacion();
+        if (localStorage.length > 0) {
+            cargarPrestamo();
+            imprimirResultado();
+            animacion();
+        } else {alert("No posee simulaciones guardadas.");}
     });
 
     $('#volver').on('click', () => {
